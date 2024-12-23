ce836c245fb48ccf053ebccf1dab1e5fe9acd11c
piyush.singhal.2004@gmail.com <piyush.singhal.2004@gmail.com>
2024-12-23 14:33:49 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,15 +1,19 @@
-from ._anvil_designer import Form1Template
-from anvil import *
 import anvil.server
 
+class Form1(Form1Template):  # Automatically generated class from the Form1 template
+    def __init__(self, **properties):
+        self.init_components(**properties)
 
-class Form1(Form1Template):
-  def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
-    self.init_components(**properties)
+    def fetch_comments_button_click(self, **event_args):
+        video_url = self.video_url_textbox.text  # Get the URL from the TextBox
+        if video_url:
+            try:
+                # Call the server function to analyze the overall sentiment
+                overall_sentiment = anvil.server.call('get_overall_sentiment', video_url)
 
-    # Any code you write here will run before the form opens.
-
-  def button_1_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    pass
+                # Display the result in the Label
+                self.result_label.text = f"Overall Sentiment: {overall_sentiment}"
+            except Exception as e:
+                self.result_label.text = f"Error: {str(e)}"
+        else:
+            self.result_label.text = "Please enter a valid YouTube URL."
